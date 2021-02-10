7bdc208bc78c6ca29e1b2f5facf978c761afda13
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2021-02-10 09:23:39 +0100

    change order of pseudonymisation and record linkage, implement consent sceleton

--- dsf-bpe-process-data-sharing/src/main/java/org/highmed/dsf/bpe/service/FilterQueryResultsByConsent.java
+++ dsf-bpe-process-data-sharing/src/main/java/org/highmed/dsf/bpe/service/FilterQueryResultsByConsent.java
@@ -1,25 +1,52 @@
 package org.highmed.dsf.bpe.service;
 
+import static org.highmed.dsf.bpe.ConstantsDataSharing.BPMN_EXECUTION_VARIABLE_QUERY_RESULTS;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
 import org.highmed.dsf.bpe.delegate.AbstractServiceDelegate;
+import org.highmed.dsf.bpe.variable.QueryResult;
+import org.highmed.dsf.bpe.variable.QueryResults;
+import org.highmed.dsf.bpe.variable.QueryResultsValues;
 import org.highmed.dsf.fhir.client.FhirWebserviceClientProvider;
 import org.highmed.dsf.fhir.task.TaskHelper;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import org.highmed.openehr.model.structure.ResultSet;
 
 public class FilterQueryResultsByConsent extends AbstractServiceDelegate
 {
-
-	private static final Logger logger = LoggerFactory.getLogger(FilterQueryResultsByConsent.class);
-
 	public FilterQueryResultsByConsent(FhirWebserviceClientProvider clientProvider, TaskHelper taskHelper)
 	{
 		super(clientProvider, taskHelper);
 	}
 
 	@Override
-	protected void doExecute(DelegateExecution execution)
+	protected void doExecute(DelegateExecution execution) throws Exception
 	{
-		logger.info(this.getClass().getName() + " doExecute called");
+		QueryResults results = (QueryResults) execution.getVariable(BPMN_EXECUTION_VARIABLE_QUERY_RESULTS);
+
+		List<QueryResult> filteredResults = filterResults(results.getResults());
+
+		execution.setVariable(BPMN_EXECUTION_VARIABLE_QUERY_RESULTS,
+				QueryResultsValues.create(new QueryResults(filteredResults)));
+	}
+
+	private List<QueryResult> filterResults(List<QueryResult> results)
+	{
+		return results.stream().map(this::filterResult).collect(Collectors.toList());
+	}
+
+	protected QueryResult filterResult(QueryResult result)
+	{
+		return QueryResult.resultSet(result.getOrganizationIdentifier(), result.getCohortId(),
+				filterResultSet(result.getResultSet()));
+	}
+
+	private ResultSet filterResultSet(ResultSet resultSet)
+	{
+		// TODO implement
+
+		return resultSet;
 	}
 }
