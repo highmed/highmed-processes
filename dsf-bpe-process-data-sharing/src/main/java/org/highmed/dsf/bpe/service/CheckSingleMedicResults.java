d0424f4b01c70fe1f2daa31f9320665bca6ca4dc
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2021-03-08 08:28:12 +0100

    adds intial implementation of check results

--- dsf-bpe-process-data-sharing/src/main/java/org/highmed/dsf/bpe/service/CheckSingleMedicResults.java
+++ dsf-bpe-process-data-sharing/src/main/java/org/highmed/dsf/bpe/service/CheckSingleMedicResults.java
@@ -1,9 +1,20 @@
 package org.highmed.dsf.bpe.service;
 
+import static org.highmed.dsf.bpe.ConstantsDataSharing.BPMN_EXECUTION_VARIABLE_QUERY_RESULTS;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 import org.camunda.bpm.engine.delegate.DelegateExecution;
+import org.highmed.dsf.bpe.ConstantsBase;
 import org.highmed.dsf.bpe.delegate.AbstractServiceDelegate;
+import org.highmed.dsf.bpe.variable.QueryResult;
+import org.highmed.dsf.bpe.variable.QueryResults;
+import org.highmed.dsf.bpe.variable.QueryResultsValues;
 import org.highmed.dsf.fhir.client.FhirWebserviceClientProvider;
 import org.highmed.dsf.fhir.task.TaskHelper;
+import org.hl7.fhir.r4.model.Task;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,6 +31,45 @@ public class CheckSingleMedicResults extends AbstractServiceDelegate
 	@Override
 	protected void doExecute(DelegateExecution execution)
 	{
-		logger.info(this.getClass().getName() + " doExecute called");
+		QueryResults results = (QueryResults) execution.getVariable(BPMN_EXECUTION_VARIABLE_QUERY_RESULTS);
+
+		Task currentTask = getCurrentTaskFromExecutionVariables();
+		List<QueryResult> filteredResults = filterErroneousResultsAndAddErrorsToCurrentTaskOutputs(results,
+				currentTask);
+
+		execution.setVariable(BPMN_EXECUTION_VARIABLE_QUERY_RESULTS,
+				QueryResultsValues.create(new QueryResults(filteredResults)));
+	}
+
+	private List<QueryResult> filterErroneousResultsAndAddErrorsToCurrentTaskOutputs(QueryResults results, Task task)
+	{
+		List<QueryResult> filteredResults = new ArrayList<>();
+		for (QueryResult result : results.getResults())
+		{
+			Optional<String> errorReason = testResultAndReturnErrorReason(result);
+			if (errorReason.isPresent())
+				addError(task, result.getCohortId(), errorReason.get());
+			else
+				filteredResults.add(result);
+		}
+
+		return filteredResults;
+	}
+
+	protected Optional<String> testResultAndReturnErrorReason(QueryResult result)
+	{
+		// TODO: implement check
+		// result size > 0
+		// other filter criteria tbd
+		return Optional.empty();
+	}
+
+	private void addError(Task task, String cohortId, String error)
+	{
+		String errorMessage = "Data sharing query result check failed for group with id '" + cohortId + "': " + error;
+		logger.info(errorMessage);
+
+		task.getOutput().add(getTaskHelper().createOutput(ConstantsBase.CODESYSTEM_HIGHMED_BPMN,
+				ConstantsBase.CODESYSTEM_HIGHMED_BPMN_VALUE_ERROR, errorMessage));
 	}
 }
