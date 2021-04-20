2e148dd45aa1e3da0f83187defc6956a62b0c595
Reto Wettstein <Reto.Wettstein@med.uni-heidelberg.de>
2021-04-20 11:50:05 +0200

    increase versions to 0.4.1 in pom, plugin and bpmn

--- dsf-bpe-process-ping/src/main/java/org/highmed/dsf/bpe/PingProcessPluginDefinition.java
+++ dsf-bpe-process-ping/src/main/java/org/highmed/dsf/bpe/PingProcessPluginDefinition.java
@@ -15,7 +15,7 @@ import ca.uhn.fhir.context.FhirContext;
 
 public class PingProcessPluginDefinition implements ProcessPluginDefinition
 {
-	public static final String VERSION = "0.4.0";
+	public static final String VERSION = "0.4.1";
 
 	@Override
 	public String getName()
